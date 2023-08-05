82e41979051130f17230f809211519c8bee14b11
Ethan Zhang <ezsland05@gmail.com>
2023-08-05 01:01:35 -0700

    GET /:id now reads from the database

--- bin/main.ml
+++ bin/main.ml
@@ -1,19 +1,36 @@
 open Ppx_yojson_conv_lib.Yojson_conv.Primitives
 
+module type DB = Caqti_lwt.CONNECTION
+
+module T = Caqti_type
+
 type url_object = { url : string } [@@deriving yojson]
+
+let get_url_from_id =
+  let query =
+    let open Caqti_request.Infix in
+    (T.string ->* T.string) "SELECT url FROM links WHERE id = ($1)"
+  in
+  fun text (module Db : DB) ->
+    let%lwt link_or_error = Db.collect_list query text in
+    Caqti_lwt.or_fail link_or_error
 
 let () =
   Dream.run @@ Dream.logger
+  @@ Dream.sql_pool "sqlite3:db.sqlite"
   @@ Dream.router
        [
          Dream.get "/:url" (fun request ->
-             Dream.html (Dream.param request "url"));
+             let url = Dream.param request "url" in
+             let%lwt full_url = Dream.sql request (get_url_from_id url) in
+             match full_url with
+             | [] -> Dream.empty `Not_Found
+             | url :: [] -> Dream.html url
+             | _ :: _ -> Dream.empty `Internal_Server_Error);
          Dream.post "/api/shorten" (fun request ->
              let%lwt body = Dream.body request in
-
              let url_object =
                body |> Yojson.Safe.from_string |> url_object_of_yojson
              in
-
              `String url_object.url |> Yojson.Safe.to_string |> Dream.json);
        ]

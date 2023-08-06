469a32a38e4346050cabf8c78a50200508205ab8
Ethan Zhang <ezsland05@gmail.com>
2023-08-05 18:19:44 -0700

    generates uuids and inserts (uuid, url) into db

--- bin/main.ml
+++ bin/main.ml
@@ -10,11 +10,20 @@ type url_object = { url : string } [@@deriving yojson]
 let get_url_from_id =
   let query =
     let open Caqti_request.Infix in
-    (T.string ->* T.string) "SELECT url FROM links WHERE id = ($1)"
+    (T.string ->* T.string) "SELECT url FROM links WHERE id = $1"
   in
-  fun text (module Db : DB) ->
-    let%lwt link_or_error = Db.collect_list query text in
+  fun uuid (module Db : DB) ->
+    let%lwt link_or_error = Db.collect_list query uuid in
     Caqti_lwt.or_fail link_or_error
+
+let add_url =
+  let query =
+    let open Caqti_request.Infix in
+    (T.(tup2 string string) ->. T.unit) "INSERT INTO links VALUES($1, $2)"
+  in
+  fun uuid url (module Db : DB) ->
+    let%lwt unit_or_error = Db.exec query (uuid, url) in
+    Caqti_lwt.or_fail unit_or_error
 
 let () =
   Dream.run @@ Dream.logger
@@ -34,7 +43,14 @@ let () =
                let url_object =
                  body |> Yojson.Safe.from_string |> url_object_of_yojson
                in
-               `String url_object.url |> Yojson.Safe.to_string |> Dream.json
+               let uuid =
+                 Uuidm.v3 Uuidm.ns_url url_object.url
+                 |> Uuidm.to_string
+                 |> Str.global_replace (Str.regexp "-") ""
+               in
+               Dream.log "Attempting to insert (%s, %s)" uuid url_object.url;
+               let%lwt () = Dream.sql request (add_url uuid url_object.url) in
+               Dream.json uuid
              with Ppx_yojson_conv_lib.Yojson_conv.Of_yojson_error (exn, _) ->
                Dream.log "Bad request: %s" (Exn.to_string exn);
                Dream.empty `Bad_Request);
